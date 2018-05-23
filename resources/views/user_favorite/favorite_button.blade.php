ce620905ca58ce01e18abdba7c5272d37e0e7692
asamin <applevr58@gmail.com>
2018-05-23 11:30:55 +0000

    add microposts button color

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,6 +1,6 @@
 @if (Auth::user()->is_favorites($micropost->id))
     {!! Form::open(['route' => ['user.unfavorites', $micropost->id], 'method' => 'delete']) !!}
-        {!! Form::submit('Unfavorite', ['class' => "btn btn-sccess btn-block"]) !!}
+        {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-block"]) !!}
     {!! Form::close() !!}
 @else
     {!! Form::open(['route' => ['user.favorites', $micropost->id], 'method' => 'post']) !!}
