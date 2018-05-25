01339560f9a968e83661dba9123e18b75ef4f0cc
asamin <applevr58@gmail.com>
2018-05-25 15:52:07 +0000

    button size

--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -1,9 +1,9 @@
 @if (Auth::user()->is_favorites($micropost->id))
     {!! Form::open(['route' => ['user.unfavorites', $micropost->id], 'method' => 'delete']) !!}
-        {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-block"]) !!}
+        {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-xs"]) !!}
     {!! Form::close() !!}
 @else
     {!! Form::open(['route' => ['user.favorites', $micropost->id], 'method' => 'post']) !!}
-        {!! Form::submit('Favorite', ['class' => "btn btn-default btn-block"]) !!}
+        {!! Form::submit('Favorite', ['class' => "btn btn-default btn-xs"]) !!}
     {!! Form::close() !!}
 @endif
